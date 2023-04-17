b0ad073dc51ba98bff7fdc7ea53619ea601395d0
betrider <tlsfpem@gmail.com>
2023-04-17 17:01:32 +0900

    예제추가

--- bin/dart_dio_example.dart
+++ bin/dart_dio_example.dart
@@ -1,26 +1,30 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 
 void main(List<String> arguments) async {
   var dio = Dio();
 
-  dio.interceptors.add(LogInterceptor());
+  dio.transformer = BackgroundTransformer()..jsonDecodeCallback = parseJson;
 
-  dio.interceptors.add(InterceptorsWrapper(
-    onRequest: (options, handler) {
-      print('onRequest');
-      return handler.next(options);
-      // return handler.resolve(Response(requestOptions:options, data:'fake data'));
-      // return handler.reject(DioError(requestOptions: options, message: 'error test'));
-    },
-    onResponse: (response, handler) {
-      print('onResponse');
-      return handler.next(response);
-    },
-    onError: (error, handler) {
-      print('onError');
-      return handler.next(error);
-    },
-  ));
+  // dio.interceptors.add(LogInterceptor());
+
+  // dio.interceptors.add(InterceptorsWrapper(
+  //   onRequest: (options, handler) {
+  //     print('onRequest');
+  //     return handler.next(options);
+  //     // return handler.resolve(Response(requestOptions:options, data:'fake data'));
+  //     // return handler.reject(DioError(requestOptions: options, message: 'error test'));
+  //   },
+  //   onResponse: (response, handler) {
+  //     print('onResponse');
+  //     return handler.next(response);
+  //   },
+  //   onError: (error, handler) {
+  //     print('onError');
+  //     return handler.next(error);
+  //   },
+  // ));
 
   // 첫번째 방법
   try {
@@ -35,7 +39,7 @@ void main(List<String> arguments) async {
   // final response2 = await dio.get('https://jsonplaceholder.typicode.com/comments', queryParameters: {'postId': 1});
   // print(response2);
 
-  // // 세번째 방법
+  // 세번째 방법
   // final response3 = await dio.request(
   //   'https://jsonplaceholder.typicode.com/comments',
   //   data: {'postId': 1},
@@ -43,4 +47,17 @@ void main(List<String> arguments) async {
   // );
   // print(response3);
 
+  // 다중 요청
+  // List<Response<dynamic>> responses = await Future.wait([dio.post('/info'), dio.get('/token')]);
+
+}
+
+/// Must be top-level function
+Map<String, dynamic> _parseAndDecode(String response) {
+  return jsonDecode(response) as Map<String, dynamic>;
+}
+
+Future<Map<String, dynamic>> parseJson(String text) async{
+  // return compute(_parseAndDecode, text); // isolate 사용시
+  return _parseAndDecode(text);
 }
